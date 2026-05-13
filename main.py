1d48d56e83bb60ec97d12cb3f0556902ce071a01
Qsor developer <qsor.github@gmail.com>
2026-05-14 05:37:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("ждите")
+import cv2
+import time
+
+def main():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        cap = cv2.VideoCapture(1)
+        if not cap.isOpened():
+            print("Ошибка: камера не найдена")
+            return
+
+    print("g - ч/б режим, s - сохранить кадр, q - выход")
+
+    fps_start = time.time()
+    frame_count = 0
+    fps = 0.0
+    gray_mode = False
+
+
+
+if __name__ == "__main__":
+    main()
